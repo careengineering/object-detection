846ebce95e2c8b82f39cd7a19f002ba63885bf3b
John DoeCesur <cesurkedix@gmail.com>
2023-10-11 11:22:58 +0300

    project 4 finished

--- Project 5 - Garden Security/Garden-Detection.py
+++ Project 5 - Garden Security/Garden-Detection.py
@@ -4,7 +4,7 @@ import cvzone
 import math
 from sort import *
 
-cap = cv2.VideoCapture("../Videos/cars.mp4")  # For Videos
+cap = cv2.VideoCapture("../Videos/people.mp4")  # For Videos
 
 model = YOLO("../Yolo-Weights/yolov8l.pt")
 
@@ -20,21 +20,23 @@ classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "trai
               "teddy bear", "hair drier", "toothbrush"
               ]
 
-mask = cv2.imread("mask_canva.png")
+mask = cv2.imread("mask.png")
 
 #Tracking
 tracker = Sort(max_age=20, min_hits=3, iou_threshold=0.3)
 
-limits =  [400,297,673,297]
+limitsUp =  [103,161,296,161]
+limitsDown =  [527,489,735,489]
 
-totalCount = []
+totalCountUp = []
+totalCountDown = []
 
 while True:
     success, img = cap.read()
     imgRegion = cv2.bitwise_and(img,mask)
 
     imgGraphics = cv2.imread("graphics.png", cv2.IMREAD_UNCHANGED)
-    img = cvzone.overlayPNG(img,imgGraphics,(0,0))
+    img = cvzone.overlayPNG(img,imgGraphics,(730,260))
     results = model(imgRegion, stream=True)
 
     detections = np.empty((0, 5))
@@ -55,9 +57,8 @@ while True:
             cls = int(box.cls[0])
             currentClass = classNames[cls]
 
-            # if it is .... and conf rect bigger than 0.3 detect objects
-            if currentClass == "car" or currentClass == "truck" or currentClass == "bus" \
-                    or currentClass == "motorbike" and conf>0.3:
+
+            if currentClass == "person" and conf>0.3:
                 # cvzone.putTextRect(img,f'{currentClass} {conf}',(max(0,x1) , max(35,y1)) , scale=0.6 , thickness=1, offset=3)
                 # cvzone.cornerRect(img, (x1,y1,w,h), l=9 , rt=5)
                 currentArray = np.array([x1,y1,x2,y2,conf])
@@ -65,7 +66,8 @@ while True:
 
 
     resultsTracker = tracker.update(detections)
-    cv2.line(img,(limits[0], limits[1]),(limits[2], limits[3]),(0,0,255),5)
+    cv2.line(img,(limitsUp[0], limitsUp[1]),(limitsUp[2], limitsUp[3]),(0,0,255),5)
+    cv2.line(img, (limitsDown[0], limitsDown[1]), (limitsDown[2], limitsDown[3]), (0, 0, 255), 5)
 
     for result in resultsTracker:
         x1,y1,x2,y2,id = result
@@ -78,13 +80,18 @@ while True:
         cx,cy = x1+w//2,y1+h//2
         cv2.circle(img,(cx,cy),5,(255,0,255),cv2.FILLED)
 
-        if limits[0] < cx < limits[2] and limits[1]-15 < cy < limits[1]+15:
-            if totalCount.count(id) == 0:
-                totalCount.append(id)
-                cv2.line(img, (limits[0], limits[1]), (limits[2], limits[3]), (0, 255, 0), 5)
+        if limitsUp[0] < cx < limitsUp[2] and limitsUp[1]-15 < cy < limitsUp[1]+15:
+            if totalCountUp.count(id) == 0:
+                totalCountUp.append(id)
+                cv2.line(img, (limitsUp[0], limitsUp[1]), (limitsUp[2], limitsUp[3]), (0, 255, 0), 5)
 
-   # cvzone.putTextRect(img, f' Count: {len(totalCount)}', (50,50))
-    cv2.putText(img,str(len(totalCount)),(255,100),cv2.FONT_HERSHEY_PLAIN,5,(50,50,255),8)
+        if limitsDown[0] < cx < limitsDown[2] and limitsDown[1]-15 < cy < limitsDown[1]+15:
+            if totalCountDown.count(id) == 0:
+                totalCountDown.append(id)
+                cv2.line(img, (limitsDown[0], limitsDown[1]), (limitsDown[2], limitsDown[3]), (0, 255, 0), 5)
+
+    cv2.putText(img,str(len(totalCountUp)),(929,345),cv2.FONT_HERSHEY_PLAIN,5,(139,195,75),7)
+    cv2.putText(img, str(len(totalCountDown)), (1191, 345), cv2.FONT_HERSHEY_PLAIN, 5, (50, 50, 230), 7)
 
     cv2.imshow ("Image", img)
     # cv2.imshow("ImageRegion", imgRegion)
